a4c0e0d2af9543dbe4ff8e2dca864437dadf4c98
samir <samermedhat227@gmail.com>
2026-07-09 18:27:09 +0300

    first release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.daily_gratitude_app"
+    namespace = "com.samir.athar"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.daily_gratitude_app"
+        applicationId = "com.samir.athar"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
